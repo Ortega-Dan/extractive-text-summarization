d5432fbc1a48eac9aa7063b60cd8460a676f7425
Dan Ortega <ortega.dan2010@gmail.com>
2021-03-29 18:22:34 -0500

    adding missing dep

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 # Install deps
 pip3 install -U pip setuptools wheel
 pip3 install spacy
+pip3 install -U scikit-learn
 # Downlod english, portuguese, and spanish models
 python3 -m spacy download pt_core_news_sm
 python3 -m spacy download en_core_web_sm
