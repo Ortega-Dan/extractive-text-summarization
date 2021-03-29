487147cf669ceaee0a5aab2d2dd4b410be3cf7d1
Dan Ortega <ortega.dan2010@gmail.com>
2021-03-29 18:17:23 -0500

    Adapting it to work with spanish

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-# Downlod english and portuguese models
-python -m spacy download pt_core_news_sm
-python -m spacy download en_core_web_sm
+# Install deps
+pip3 install -U pip setuptools wheel
+pip3 install spacy
+# Downlod english, portuguese, and spanish models
+python3 -m spacy download pt_core_news_sm
+python3 -m spacy download en_core_web_sm
+python3 -m spacy download es_core_news_md
 
--- summarization.py
+++ summarization.py
@@ -3,66 +3,65 @@ from spacy.lang.pt.stop_words import STOP_WORDS
 from sklearn.feature_extraction.text import CountVectorizer
 import argparse
 
+
 def summarization(args):
 
     with open(args.original_text, "r", encoding="utf-8") as f:
         text = " ".join(f.readlines())
 
-    if args.language == 'portuguese':
-        import pt_core_news_sm
-        nlp = pt_core_news_sm.load()
+    if args.language == 'spanish':
+        import es_core_news_md
+        nlp = es_core_news_md.load()
     else:
         import en_core_web_sm
         nlp = en_core_web_sm.load()
-    
-    
+
     doc = nlp(text)
 
+    corpus = [sent.text.lower() for sent in doc.sents]
 
-    corpus = [sent.text.lower() for sent in doc.sents ]
-    
-    cv = CountVectorizer(stop_words=list(STOP_WORDS))   
-    cv_fit=cv.fit_transform(corpus)    
-    word_list = cv.get_feature_names();    
-    count_list = cv_fit.toarray().sum(axis=0)    
+    cv = CountVectorizer(stop_words=list(STOP_WORDS))
+    cv_fit = cv.fit_transform(corpus)
+    word_list = cv.get_feature_names()
+    count_list = cv_fit.toarray().sum(axis=0)
 
     """
     The zip(*iterables) function takes iterables as arguments and returns an iterator. 
     This iterator generates a series of tuples containing elements from each iterable. 
     Let's convert these tuples to {word:frequency} dictionary"""
 
-    word_frequency = dict(zip(word_list,count_list))
+    word_frequency = dict(zip(word_list, count_list))
 
-    val=sorted(word_frequency.values())
+    val = sorted(word_frequency.values())
 
     # Check words with higher frequencies
-    higher_word_frequencies = [word for word,freq in word_frequency.items() if freq in val[-3:]]
+    higher_word_frequencies = [
+        word for word, freq in word_frequency.items() if freq in val[-3:]]
     print("\nWords with higher frequencies: ", higher_word_frequencies)
 
     # gets relative frequencies of words
     higher_frequency = val[-1]
-    for word in word_frequency.keys():  
+    for word in word_frequency.keys():
         word_frequency[word] = (word_frequency[word]/higher_frequency)
 
-
     # SENTENCE RANKING: the rank of sentences is based on the word frequencies
-    sentence_rank={}
+    sentence_rank = {}
     for sent in doc.sents:
-        for word in sent :       
-            if word.text.lower() in word_frequency.keys():            
+        for word in sent:
+            if word.text.lower() in word_frequency.keys():
                 if sent in sentence_rank.keys():
-                    sentence_rank[sent]+=word_frequency[word.text.lower()]
+                    sentence_rank[sent] += word_frequency[word.text.lower()]
                 else:
-                    sentence_rank[sent]=word_frequency[word.text.lower()]
+                    sentence_rank[sent] = word_frequency[word.text.lower()]
             else:
                 continue
 
-    top_sentences=(sorted(sentence_rank.values())[::-1])
-    top_sent=top_sentences[:args.nb_sentences]
+    top_sentences = (sorted(sentence_rank.values())[::-1])
+    top_sent = top_sentences[:args.nb_sentences]
 
     # Mount summary
-    summary=[]
-    for sent,strength in sentence_rank.items():  
+    summary = []
+    for sent, strength in sentence_rank.items():
         if strength in top_sent:
             summary.append(sent)
 
@@ -70,19 +69,20 @@ def summarization(args):
     return text, summary
 
 
-if __name__ == '__main__':        
+if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Extractive text summarization')
+    parser = argparse.ArgumentParser(
+        description='Extractive text summarization')
 
     parser.add_argument('--nb_sentences', dest='nb_sentences',
-                    default=3, type=int, help='Number of sentences in summary (default: 3)')
+                        default=3, type=int, help='Number of sentences in summary (default: 3)')
 
     parser.add_argument('--original_text', dest='original_text',
-                    default="original_text.txt", help='Path to original text')
-    
+                        default="original_text.txt", help='Path to original text')
+
     parser.add_argument('--language', dest='language',
-                    default="portuguese", help='Language of summary. Supports portuguese or english')
-    
+                        default="portuguese", help='Language of summary. Supports portuguese or english')
+
     args = parser.parse_args()
 
     text, summary = summarization(args)
@@ -94,4 +94,4 @@ if __name__ == '__main__':
     print("\nSummary:")
 
     for i in summary:
-        print(i,end=" ")
+        print(i, end=" ")
